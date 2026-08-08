f917138de5d16b8ba4325917084cbf1b6c581d2e
Augusto Lima Jardim <augustolimajardim@gmail.com>
2026-08-07 22:19:30 -0300

    feat: cria um card para exibir informações de um produto

--- src/app/features/list/list.ts
+++ src/app/features/list/list.ts
@@ -1,10 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { Products } from '../../shared/services/products.service';
 import { Product } from '../../shared/interfaces/product.interface';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-list',
-  imports: [],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './list.html',
   styleUrl: './list.scss',
 })
