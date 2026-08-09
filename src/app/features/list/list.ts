2105983fc86a1a41e87bc9e4f2bf9e5b2b4b37a6
Augusto Lima Jardim <augustolimajardim@gmail.com>
2026-08-08 22:07:56 -0300

    refactor: refatora listagem de produtos criando um componente para renderizá-lo

--- src/app/features/list/list.ts
+++ src/app/features/list/list.ts
@@ -1,12 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { Products } from '../../shared/services/products.service';
 import { Product } from '../../shared/interfaces/product.interface';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
+import { Card } from './components/card/card';
 
 @Component({
   selector: 'app-list',
-  imports: [MatCardModule, MatButtonModule],
+  imports: [Card],
   templateUrl: './list.html',
   styleUrl: './list.scss',
 })
