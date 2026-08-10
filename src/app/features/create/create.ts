2c16e70f9d10f00a057a553c2e4e5483f5cd44bb
Augusto Lima Jardim <augustolimajardim@gmail.com>
2026-08-10 12:17:51 -0300

    fix: corrige cor do botão de criar produto

--- src/app/features/create/create.ts
+++ src/app/features/create/create.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-create',
-  imports: [],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
   templateUrl: './create.html',
   styleUrl: './create.scss',
 })
-export class Create {}
+export class Create {
+  form = new FormGroup({
+    title: new FormControl<string>('', {
+      nonNullable: true,
+      validators: Validators.required }),
+  });
+
+  onSubmit() {
+    if (this.form.controls.title.valid) {
+      console.log(this.form.value);
+    }
+  }
+}
