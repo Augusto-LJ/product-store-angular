b6daa9a9a694a18713027cfb42242fd1d390a707
Augusto Lima Jardim <augustolimajardim@gmail.com>
2026-08-10 20:31:00 -0300

    feat: implementa funcionalidade de salvar novo produto

--- src/app/features/create/create.ts
+++ src/app/features/create/create.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { Products } from '../../shared/services/products.service';
 
 @Component({
   selector: 'app-create',
@@ -11,13 +12,26 @@ import { MatInputModule } from '@angular/material/input';
   styleUrl: './create.scss',
 })
 export class Create {
+  productsService = inject(Products);
+
   form = new FormGroup({
     title: new FormControl<string>('', {
       nonNullable: true,
-      validators: Validators.required }),
+      validators: Validators.required
+    }),
+    description: new FormControl<string>('', {
+      nonNullable: true,
+      validators: Validators.required
+    })
   });
 
   onSubmit() {
-    this.form.controls.title.value;
+    this.productsService.post({
+      title: this.form.controls.title.value,
+      description: this.form.controls.description.value,
+    })
+    .subscribe(() => {
+      alert('Sucesso');
+    });
   }
 }
--- src/app/shared/services/products.service.ts
+++ src/app/shared/services/products.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Service } from '@angular/core';
 import { Product } from '../interfaces/product.interface';
+import { ProductPayload } from '../interfaces/payload.product.interface';
 
 @Service()
 export class Products {
@@ -8,5 +9,9 @@ export class Products {
 
     getAll() {
         return this.httpClient.get<Product[]>('/api/products');
+    }
+
+    post(payload: ProductPayload) {
+        return this.httpClient.post('/api/products', payload);
     }
 }
