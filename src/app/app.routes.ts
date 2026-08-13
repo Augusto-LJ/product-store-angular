9d6596d2e421de6339aa804087e9bf09b47b776e
Augusto Lima Jardim <augustolimajardim@gmail.com>
2026-08-12 21:09:58 -0300

    feat: adicionar página de edição de produto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { List } from './features/list/list';
-import { Create } from './features/create/create';
 
 export const routes: Routes = [
     {
@@ -10,5 +9,9 @@ export const routes: Routes = [
     {
         path: 'create-product',
         loadComponent: () => import('./features/create/create').then(m => m.Create)
+    },
+    {
+        path: 'edit-product',
+        loadComponent: () => import('./features/edit/edit').then(m => m.Edit)
     }
 ];
--- src/app/features/list/list.ts
+++ src/app/features/list/list.ts
@@ -2,7 +2,7 @@ import { Component, inject } from '@angular/core';
 import { Products } from '../../shared/services/products.service';
 import { Product } from '../../shared/interfaces/product.interface';
 import { Card } from './components/card/card';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 
 @Component({
@@ -17,10 +17,15 @@ export class List {
   products: Product[] = [];
 
   productsService = inject(Products);
+  router = inject(Router);
 
   ngOnInit() {
     this.productsService.getAll().subscribe((products) => {
       this.products = products;
     });
+  }
+
+  onEdit() {
+    this.router.navigateByUrl('/edit-product');
   }
 }
